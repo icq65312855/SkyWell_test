c3459aff67fdff6c1286194005bd0f939385601b
stepan <stepan.mozyra@imtf.ch>
2018-11-22 17:17:59 +0200

    use ASCIITable

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import test.ASCIITable;
+
 public class Main {
 
+    private static final String[] HEADER = new String[]{"ID", "NAME", "ACCOUNT", "DEBT"};
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[][] data = {
+            {"1", "Stepan", "123.00"},
+            {"2", "Ivan", "23.00", "32.00"},
+            {"3", "John", "0.00", "321.00", "active"}
+        };
+
+        ASCIITable.getInstance().printTable(HEADER, data);
     }
 }
