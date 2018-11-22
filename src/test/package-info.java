973f6b9acc88acc35d3d22b770b5e3bf7bacb6ba
stepan <stepan.mozyra@imtf.ch>
2018-11-22 16:55:56 +0200

    add ASCIITable

---
+++ src/test/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Ascii table package contains utilities to display nice formatted ascii table.
+ * <p>
+ * The code is imported from: http://bethecoder.com/open-source/ascii-art/btc-ascii-table-1.0.html
+ * <p>
+ * The original code is distributed under Apache Licence.
+ *
+ * @author K Venkata Sudhakar (kvenkatasudhakar@gmail.com)
+ */
+package test;
